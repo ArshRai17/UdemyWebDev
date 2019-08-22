ef22ea1a8f5a98226cb31b91f16c280a172b0b20
Arsh Rai <arshabhirai@gmail.com>
2019-08-22 16:26:08 -0400

    Authenticaion Part 3

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -11,7 +11,7 @@ mongoose.connect("mongodb://localhost/authentication", {useNewUrlParser: true});
 
 var app = express();
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "Cutest boy in the world",
     resave: false,
@@ -24,6 +24,31 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+
+//==============
+//ROUTES
+//==============
+
+
+// Auth Routes
+//Show signup form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//Handling sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register')
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
 
 app.get("/", function(req, res){
     res.render("home");
---
+++ Authentication/AuthDemo/readme.md
@@ -0,0 +1,14 @@
+#Auth CodeAlong Part 1
+* Set up folder structure
+* Install needed packages
+* Add root route and template
+* Add secret route and template
+
+#Auth CodeAlong Part 2
+* Create User model
+* Configure passport
+
+#Auth CodeAlong Part 3
+* Add Register routes
+* Add Register form
+
