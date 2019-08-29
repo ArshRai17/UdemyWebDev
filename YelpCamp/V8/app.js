dfa759eaefa896a99cdd1f56641bc41360386476
Arsh Rai <arshabhirai@gmail.com>
2019-08-29 08:48:53 -0400

    YelpCamp Clean up Comment

--- YelpCamp/V8/app.js
+++ YelpCamp/V8/app.js
@@ -18,7 +18,8 @@ mongoose.connect("mongodb://localhost/yelp_camp_V8", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
-seedDB();
+
+// seedDB(); //see the database
 
 //PASSPORT CONFIGURATION
 app.use(require("express-session")({
