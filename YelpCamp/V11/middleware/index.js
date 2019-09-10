72c7afb16242eab6fa6fb1e327db2684ee06ea0d
Arsh Rai <arshabhirai@gmail.com>
2019-09-10 08:26:22 -0400

    Flash Installation

--- YelpCamp/V11/middleware/index.js
+++ YelpCamp/V11/middleware/index.js
@@ -46,6 +46,7 @@ middlewareObj.isLoggedIn = function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
+    req.flash("error", "Please Login FIrst!")
     res.redirect("/login");
 }
 
